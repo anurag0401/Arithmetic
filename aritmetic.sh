2d6e1cfe6b21c343d45196ebf6741fd945b455d0
“anurag0401” <xoom.aj@gmail.com>
2020-10-20 15:46:19 +0530

    Added a program to store the values in dictionary

--- aritmetic.sh
+++ aritmetic.sh
@@ -13,3 +13,11 @@ val3=$(( c+a/b ))
 echo $val3
 val4=$(( a%b+c ))
 echo $val4
+
+declare -A Dict
+Dict[1]=$val1
+Dict[2]=$val2
+Dict[3]=$val3
+Dict[4]=$val4
+
+echo "dictionary values are:" ${Dict[@]}
